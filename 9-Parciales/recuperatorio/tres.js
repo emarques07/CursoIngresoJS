7aa4184d30f43174453026b16f06ce08748a6963
emarques07 <36174180+emarques07@users.noreply.github.com>
2018-02-06 22:17:52 -0300

    Add files via upload

--- 9-Parciales/recuperatorio/tres.js
+++ 9-Parciales/recuperatorio/tres.js
@@ -1,4 +1,19 @@
 function Mostrar()
 {
+var precio1, precio2, precio3;
+var resultado;
+var promedio;
 
-}
+precio1=document.getElementById('precioUno').value;
+precio2=document.getElementById('precioDos').value;
+precio3=document.getElementById('precioTres').value;
+
+precio1=parseInt(precio1);
+precio2=parseInt(precio2);
+precio3=parseInt(precio3);
+
+resultado=precio1+precio2+precio3;
+promedio=resultado/3;
+
+alert("la suma de los precios es: " + resultado+ " El promedio es " +promedio);
+} 
