fce2fdc2fb6fd3067eadb2860bf60fd46fba7e6c
emarques07 <36174180+emarques07@users.noreply.github.com>
2018-02-07 01:27:48 -0300

    ejercicios 1-10

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,7 +2,23 @@ function Mostrar()
 {
 //tomo la edad  
 
-
-
-
+	var edad
+	edad=document.getElementById('edad').value;
+	if(edad<13)
+		{
+			alert("niño");
+		}
+		else
+		{
+			if(edad>18)
+			{
+				alert("mayor");
+			}
+			else
+			{
+				alert("adolecente");
+			}
+		}
+	
+	
 }//FIN DE LA FUNCIÓN
